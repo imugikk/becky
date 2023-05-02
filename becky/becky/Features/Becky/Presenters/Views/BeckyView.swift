1bd0062c0e1c2ad1c010910ec75b492646bec273
ugik <ugyparaptughessa@gmail.com>
2023-05-02 16:41:47 +0700

    boarding and becky

---
+++ becky/becky/Features/Becky/Presenters/Views/BeckyView.swift
@@ -0,0 +1,67 @@
+//
+//  BeckyView.swift
+//  becky
+//
+//  Created by Paraptughessa Premaswari on 02/05/23.
+//
+
+import SwiftUI
+
+struct BeckyView: View {
+    @State private var product: String = ""
+    @State private var placeholder: String = ""
+    
+    let nama: String
+    
+    var body: some View {
+        NavigationView{
+            VStack (alignment: .leading){
+                Spacer()
+                Text("Hello, \(Text(nama).font(.poppinsItalicLarge).underline())!").font(.poppinsSemiBoldLarge).foregroundColor(Color.red).padding(.bottom,1)
+                Text("Shall we assess your shopping cart?").font(.poppinsSemiBoldLarge).foregroundColor(Color.red).padding(0.0)
+                Text("*****").font(.poppinsLightLarge).fontWeight(.light).foregroundColor(Color.red)
+                
+                HStack{
+                    ZStack(alignment: .leading) {
+                        if product.isEmpty {
+                            Text("Insert product name here..") .foregroundColor(Color.red.opacity(0.4)).font(.poppinsRegular)
+                        
+                        }
+                        
+                        TextField("", text: $product).foregroundColor(Color.red).font(.poppinsRegular)
+                    }.padding(.horizontal, 15) .padding(.vertical, 8).overlay( RoundedRectangle(cornerRadius: 20).stroke(Color.red, lineWidth: 1) )
+                    
+                    if product.isEmpty { Button(action: {
+                        
+                    }) {
+                        Image(systemName: "arrow.right").padding(.vertical, 7).padding(.horizontal, 8)
+                    }
+                    .padding(7)
+                    .background(.red.opacity(0.4))
+                    .foregroundColor(.red)
+                        .cornerRadius(100)
+                    } else {
+                        Button{
+
+                        } label: {
+                            NavigationLink (destination: ChatView().navigationBarBackButtonHidden(true)){
+                                Image(systemName: "arrow.right").padding(.vertical, 7).padding(.horizontal, 8)
+                            }
+                        }
+                        .padding(7)
+                        .background(.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(100)
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct BeckyView_Previews: PreviewProvider {
+    static var previews: some View {
+        BeckyView(nama: "")
+    }
+}
---
+++ becky/becky/Features/Boarding/Presenters/Views/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  becky
+//
+//  Created by Paraptughessa Premaswari on 28/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name: String = ""
+    @State private var placeholder: String = ""
+    
+    var body: some View {
+        NavigationView{
+            VStack (alignment: .leading){
+                Spacer()
+                Text("Hello, I'm \(Text("Becky").font(.poppinsItalicLarge).underline())!").font(.poppinsSemiBoldLarge).foregroundColor(Color.red).padding(.bottom,1)
+                Text("I will assist you in tackling your impulsiveness.").font(.poppinsSemiBoldLarge).foregroundColor(Color.red).padding(0.0)
+                Text("*****").font(.poppinsLightLarge).fontWeight(.light).foregroundColor(Color.red)
+                
+                HStack{
+                    ZStack(alignment: .leading) {
+                        if name.isEmpty {
+                            Text("What's Your Name") .foregroundColor(Color.red.opacity(0.4)).font(.poppinsRegular)
+                        
+                        }
+                        
+                        TextField("", text: $name).foregroundColor(Color.red).font(.poppinsRegular)
+                    }.padding(.horizontal, 15) .padding(.vertical, 8).overlay( RoundedRectangle(cornerRadius: 20).stroke(Color.red, lineWidth: 1) )
+                    
+                    if name.isEmpty { Button(action: {
+                        
+                    }) {
+                        Image(systemName: "arrow.right").padding(.vertical, 7).padding(.horizontal, 8)
+                    }
+                    .padding(7)
+                    .background(.red.opacity(0.4))
+                    .foregroundColor(.red)
+                        .cornerRadius(100)
+                    } else {
+                        Button{
+
+                        } label: {
+                            NavigationLink (destination: BeckyView(nama: name).navigationBarBackButtonHidden(true)){
+                                Image(systemName: "arrow.right").padding(.vertical, 7).padding(.horizontal, 8)
+                            }
+                        }
+                        .padding(7)
+                        .background(.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(100)
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
