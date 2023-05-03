bdb63f23aedf9cd7df69dff8a8a96d33617e7eb8
ugik <ugyparaptughessa@gmail.com>
2023-05-03 10:22:53 +0700

    update boarding

---
+++ becky/becky/Features/Chat/Presenters/Views/ChatView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatView.swift
+//  becky
+//
+//  Created by Paraptughessa Premaswari on 02/05/23.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView()
+    }
+}
