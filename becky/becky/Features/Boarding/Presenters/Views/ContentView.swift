1fca15fddf4b0f0e0166d3d01532a8fcb381a1aa
Febriandianto <prabowo.febrian@gmail.com>
2023-05-08 00:42:38 +0700

    uji coba 2

--- becky/becky/Features/Boarding/Presenters/Views/ContentView.swift
+++ becky/becky/Features/Boarding/Presenters/Views/ContentView.swift
@@ -25,99 +25,103 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             ZStack {
-                
-                VStack (alignment: .leading) {
-                    Image("Becky_Frontpage")
-                        .resizable()
-                        .scaledToFit()
-                        .scaleEffect(0.6)
-                        .offset(x: -75, y: -60)
-                    VStack (alignment: .leading){
+                VStack{
+                    HStack{
+                        Image("Becky_Frontpage")
+                            .resizable()
+                            .scaledToFit()
+                            .scaleEffect(1.3)
+                            .offset(x: -75, y: 250)
+                        
+                    }
+                    .offset(y:-130)
+                    
+                    HStack{
                         
                         Image("hello")
-                        
-                        VStack{
-                            if showText{
-                                Text("Hello, I'm \(Text("Becky").font(.poppinsItalicLarge).underline())!").font(.poppinsSemiBoldLarge)
-//                                        .foregroundColor(Color.red)
-                                    .foregroundColor(isDarkMode ? .red : .red)
-                                    .padding(.bottom,1).frame(height: 15)
-                                    .transition(AnyTransition.opacity.animation(Animation.easeInOut(duration: 1.3)))
-                            } else {
-                                Text("Hello, I'm \(Text("Becky").font(.poppinsItalicLarge).underline())!").font(.poppinsSemiBoldLarge)
-                                    .foregroundColor(isDarkMode ? .black : .white) // update foreground color
-//                                        .foregroundColor(Color.white)
-                                    .padding(.bottom,1).frame(height: 15)
-                            }
-                        }.onAppear{
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                                withAnimation{
-                                    self.showText = true
-                                }
+                    }
+                    .offset(x:-130, y:100)
+                    HStack{
+                        if showText{
+                            Text("Hello, I'm \(Text("Becky").font(.poppinsItalicLarge).underline())!").font(.poppinsSemiBoldLarge)
+                            //                                        .foregroundColor(Color.red)
+                                .foregroundColor(isDarkMode ? .red : .red)
+                                .padding(.bottom,1).frame(height: 15)
+                                .transition(AnyTransition.opacity.animation(Animation.easeInOut(duration: 1.3)))
+                        } else {
+                            Text("Hello, I'm \(Text("Becky").font(.poppinsItalicLarge).underline())!").font(.poppinsSemiBoldLarge)
+                                .foregroundColor(isDarkMode ? .black : .white) // update foreground color
+                            //                                        .foregroundColor(Color.white)
+                                .padding(.bottom,1).frame(height: 15)
+                        }
+                    }
+                    .offset(x:-35, y:100)
+                    .onAppear{
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            withAnimation{
+                                self.showText = true
                             }
                         }
-                    }.offset(x: 15, y: -180)
+                    }
                     
-                }
-                
-                VStack (alignment: .leading){
                     HStack{
                         Button(action: {
-                          // TOGGLE APPEARANCE
-                          isDarkMode.toggle()
+                            // TOGGLE APPEARANCE
+                            isDarkMode.toggle()
                         }, label: {
-                          Image(systemName: isDarkMode ? "moon.circle.fill" :  "moon.circle")
-                            .resizable()
-                            .frame(width: 24, height: 24)
-                            .font(.system(.title, design: .rounded))
+                            Image(systemName: isDarkMode ? "moon.circle.fill" :  "moon.circle")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .font(.system(.title, design: .rounded))
                         })
                     }
                     .padding()
                     .preferredColorScheme(isDarkMode ? .dark : .light) // set preferredColorScheme
+                    .offset(x:-150, y:-300)
                     
-                    Spacer()
+//                    Spacer()
                     
-    
-                    ZStack (alignment: .leading){
-                        VStack{
-//                            VStack (alignment: .leading){
-//
-//                                Image("hello")
-//
-//                                VStack{
-//                                    if showText{
-//                                        Text("Hello, I'm \(Text("Becky").font(.poppinsItalicLarge).underline())!").font(.poppinsSemiBoldLarge)
-//    //                                        .foregroundColor(Color.red)
-//                                            .foregroundColor(isDarkMode ? .red : .red)
-//                                            .padding(.bottom,1).frame(height: 15)
-//                                            .transition(AnyTransition.opacity.animation(Animation.easeInOut(duration: 1.3)))
-//                                    } else {
-//                                        Text("Hello, I'm \(Text("Becky").font(.poppinsItalicLarge).underline())!").font(.poppinsSemiBoldLarge)
-//                                            .foregroundColor(isDarkMode ? .black : .white) // update foreground color
-//    //                                        .foregroundColor(Color.white)
-//                                            .padding(.bottom,1).frame(height: 15)
-//                                    }
-//                                }.onAppear{
-//                                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-//                                        withAnimation{
-//                                            self.showText = true
-//                                        }
-//                                    }
-//                                }
-//                            }
-                        }
-                        
-//                        Rectangle()
-//                            .fill(isDarkMode ? Color.black : Color.white)
-//                            .frame(width: 100, height: 130)
-//                            .offset(offset)
-//                            .animation(Animation.easeInOut(duration: 2))
-//                            .opacity(opacity)
-                    }
-                    .onAppear {
-                        offset = CGSize(width: 0, height: 120)}
                     
-                    VStack{
+                    //                    ZStack (alignment: .leading){
+                    //                        VStack{
+                    ////                            VStack (alignment: .leading){
+                    ////
+                    ////                                Image("hello")
+                    ////
+                    ////                                VStack{
+                    ////                                    if showText{
+                    ////                                        Text("Hello, I'm \(Text("Becky").font(.poppinsItalicLarge).underline())!").font(.poppinsSemiBoldLarge)
+                    ////    //                                        .foregroundColor(Color.red)
+                    ////                                            .foregroundColor(isDarkMode ? .red : .red)
+                    ////                                            .padding(.bottom,1).frame(height: 15)
+                    ////                                            .transition(AnyTransition.opacity.animation(Animation.easeInOut(duration: 1.3)))
+                    ////                                    } else {
+                    ////                                        Text("Hello, I'm \(Text("Becky").font(.poppinsItalicLarge).underline())!").font(.poppinsSemiBoldLarge)
+                    ////                                            .foregroundColor(isDarkMode ? .black : .white) // update foreground color
+                    ////    //                                        .foregroundColor(Color.white)
+                    ////                                            .padding(.bottom,1).frame(height: 15)
+                    ////                                    }
+                    ////                                }.onAppear{
+                    ////                                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    ////                                        withAnimation{
+                    ////                                            self.showText = true
+                    ////                                        }
+                    ////                                    }
+                    ////                                }
+                    ////                            }
+                    //                        }
+                    //
+                    ////                        Rectangle()
+                    ////                            .fill(isDarkMode ? Color.black : Color.white)
+                    ////                            .frame(width: 100, height: 130)
+                    ////                            .offset(offset)
+                    ////                            .animation(Animation.easeInOut(duration: 2))
+                    ////                            .opacity(opacity)
+                    //                    }
+                    //                    .onAppear {
+                    //                        offset = CGSize(width: 0, height: 120)}
+                    
+                    HStack{
                         if showText {
                             Text(text)
                                 .font(.poppinsSemiBoldLarge)
@@ -127,11 +131,11 @@ struct ContentView: View {
                                 .onAppear {
                                     if finalText == "I will assist you in tackling your impulsiveness." {
                                         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                                            typeWriter()
-                                                        }
-                                                    } else {
-                                                        typeWriter()
-                                                    }
+                                            typeWriter()
+                                        }
+                                    } else {
+                                        typeWriter()
+                                    }
                                 }
                         } else {
                             Text(text)
@@ -139,15 +143,20 @@ struct ContentView: View {
                                 .foregroundColor(Color.white)
                                 .padding(0.0)
                         }
+                    }.offset(x:-18, y: 20)
+                        .padding()
+                        .frame(width: 350, height: 200)
+                    
+                    HStack {
+                        Text("*****").font(.poppinsLightLarge).fontWeight(.light).foregroundColor(Color.red)
+                            .offset(x:-133, y:-25)
                     }
-                
-                    Text("*****").font(.poppinsLightLarge).fontWeight(.light).foregroundColor(Color.red)
                     
                     HStack{
                         ZStack(alignment: .leading) {
                             if name.isEmpty {
                                 Text("What's your name?") .foregroundColor(Color.red.opacity(0.75)).font(.poppinsRegular)
-                            
+                                
                             }
                             
                             TextField("", text: $name)
@@ -165,10 +174,10 @@ struct ContentView: View {
                         .padding(7)
                         .background(.red.opacity(0.4))
                         .foregroundColor(.red)
-                            .cornerRadius(100)
+                        .cornerRadius(100)
                         } else {
                             Button{
-
+                                
                             } label: {
                                 NavigationLink (destination: BeckyView(nama: name).navigationBarBackButtonHidden(true)){
                                     Image(systemName: "arrow.right").padding(.vertical, 7).padding(.horizontal, 8)
@@ -182,8 +191,11 @@ struct ContentView: View {
                             .cornerRadius(100)
                         }
                     }
+                    .offset(y:-30)
+                        .padding()
+                    
+                    
                 }
-                .padding()
             }
         }
     }
